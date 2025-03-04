d86dfa51ad6cd77243fbbb55cd52d571074a7cc4
WahyoeeeHD <wahyuupunya@gmail.com>
2025-03-04 22:51:21 +0800

    Delete notes-app directory

--- notes-app/src/scripts/app.js
+++
@@ -1,7 +0,0 @@
-import './components/index.js';
-import { home } from './view/main.js';
-import '../styles/style.css';
-
-document.addEventListener('DOMContentLoaded', () => {
-  home();
-});
--- notes-app/src/scripts/components/index.js
+++
@@ -1,4 +0,0 @@
-import './note-app.js';
-import './note-item.js';
-import './app-bar.js';
-import './footer-bar.js';
